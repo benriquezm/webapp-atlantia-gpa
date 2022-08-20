c3c56964524e5003a89d509f7f259dd1b3671958
Ing. Benito Enríquez Mora <enriquez.ico@gmail.com>
2022-08-20 00:42:10 -0500

    feat(mockup-pie-chart): add dependencies apexcharts and react-apexcharts.

--- src/components/modules/Charts/PieChart/PieChart.tsx
+++ src/components/modules/Charts/PieChart/PieChart.tsx
@@ -1,5 +1,18 @@
+import Chart from 'react-apexcharts';
+
+import { PieChartStyle } from './styles';
+
 const PieChart = () => {
-	return <div>Show PieChart.</div>;
+	const series = [44, 55, 41, 17, 15];
+	const options = {
+		labels: ['Apple', 'Mango', 'Orange', 'Watermelon'],
+	};
+
+	return (
+		<PieChartStyle>
+			<Chart options={options} series={series} type='pie' />
+		</PieChartStyle>
+	);
 };
 
 export default PieChart;
---
+++ src/components/modules/Charts/PieChart/styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const PieChartStyle = styled.div`
+	/* Rectangle 3 */
+
+	box-sizing: border-box;
+
+	position: relative;
+	width: 500px;
+	height: 407px;
+	left: 915px;
+	top: 217px;
+
+	background: #ffffff;
+	border: 1px solid #e9e9e9;
+`;
+
+export const PieChartWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
